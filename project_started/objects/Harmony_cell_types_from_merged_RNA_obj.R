e990e4cb184baba999b35102c317384f4a71a45b
Alla Karpova <allakarpova@allas-mbp.wucon.wustl.edu>
2023-10-12 16:58:51 -0500

    run harmony on patients

---
+++ project_started/objects/Harmony_cell_types_from_merged_RNA_obj.R
@@ -0,0 +1,131 @@
+# Recluster cell types 
+###libraries
+##################
+library(future)
+
+#plan("multicore", workers = 20)
+#options(future.globals.maxSize = 100 * 1024 ^ 3)
+
+suppressMessages(library(Signac))
+suppressMessages(library(Seurat))
+suppressMessages(library(GenomeInfoDb))
+suppressMessages(library(ggplot2))
+suppressMessages(library(RColorBrewer))
+suppressMessages(library(tidyverse))
+set.seed(1234)
+
+suppressMessages(library(dplyr))
+suppressMessages(library(data.table))
+
+suppressMessages(library(EnsDb.Hsapiens.v86))
+suppressMessages(library(GenomicRanges))
+suppressMessages(library(future))
+suppressMessages(library(optparse))
+suppressMessages(library(BSgenome.Hsapiens.UCSC.hg38))
+
+suppressMessages(library(googlesheets4))
+suppressMessages(library(stringr))
+suppressMessages(library(doParallel))
+suppressMessages(library(harmony))
+
+runHarmonyNormalization <- function(obj, dims=30, column = 'Patient_ID') {
+  
+  obj[["percent.mt"]] <- PercentageFeatureSet(obj, pattern = "^MT-")
+  s.genes <- cc.genes$s.genes
+  g2m.genes <- cc.genes$g2m.genes
+  
+  obj <- CellCycleScoring(obj, s.features = s.genes, g2m.features = g2m.genes, set.ident = F)
+  
+  obj <- obj %>%
+    SCTransform(
+      assay = 'RNA',
+      vars.to.regress =  c("nCount_RNA", "percent.mt", "S.Score", "G2M.Score"),
+      conserve.memory = T,
+      return.only.var.genes = T,
+      verbose = FALSE
+    )
+  obj <- obj %>%
+    RunPCA(assay = 'SCT', do.print = FALSE) %>%
+    RunHarmony(column, reduction = 'pca', assay.use = 'SCT') %>%
+    FindNeighbors(reduction = "harmony", dims = 1:dims) %>%
+    FindClusters(verbose = FALSE, resolution = 1.2, algorithm = 4,
+                 method='igraph') %>%
+    RunUMAP(reduction = "harmony", dims = 1:dims)
+  
+  obj <- NormalizeData(obj, assay = 'RNA')
+  
+  return(obj)
+  
+}
+
+
+###options###
+######################
+option_list = list(
+  make_option(c("-i", "--input.object"),
+              type="character",
+              default=NULL, 
+              help="path to merged object",
+              metavar="character"),
+  make_option(c("-o", "--output"),
+              type="character",
+              default="./", 
+              help="output folder path",
+              metavar="character"),
+  make_option(c("-e", "--extra"),
+              type="character",
+              default="./", 
+              help="add unique string identifier for your data",
+              metavar="character"),
+  make_option(c("-m","--metadata.file"),
+              type="character",
+              default=NULL,
+              help = "path to metadats file with cell types, make cell barcodes in the 1st column",
+              metavar="character"),
+  make_option(c("-c","--cell_type_column"),
+              type="character",
+              default='cell_type',
+              help = "column in the metadata with most recent cell types",
+              metavar="character")
+  
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+###################################
+
+
+# read in initial arguments
+input.path <- opt$input.object
+out_path <- opt$output
+add_filename <- opt$extra
+meta.path <- opt$metadata.file
+cell_column <- opt$cell_type_column
+
+dir.create(out_path, showWarnings = F)
+setwd(out_path)
+
+filter <- dplyr::filter
+select <- dplyr::select
+my.metadata <- fread(meta.path, data.table = F, header = TRUE) %>% 
+  data.frame(row.names = 1, check.rows = F, check.names = F)
+
+panc.my <- readRDS(input.path)
+panc.my <- AddMetaData(panc.my, my.metadata)
+DefaultAssay(panc.my) <- 'RNA'
+panc.my <- DietSeurat(panc.my, assays = 'RNA')
+
+panc.my <- subset(x = panc.my, cells = rownames(dplyr::filter(panc.my@meta.data, !grepl('Doubl', .data[[cell_column]]))))
+
+ct<- unique(panc.my@meta.data[[cell_column]])
+print(ct)
+print(dim(panc.my))
+
+int <- runHarmonyNormalization(panc.my)
+
+cat('saving the object...\n')
+saveRDS(int,  saveRDS(int,  paste0("Harmony_RNA_", add_filename,"_",ct, "_no_doublets.rds")))
+
+
+
+
