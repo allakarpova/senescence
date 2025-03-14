c991a363c539a530fe31061a8f36ae2c0ea3ebff
Alla Karpova <allakarpova@Allas-new-MacBook-Pro.local>
2025-03-14 16:02:37 -0500

    recluster cell types from xenium

---
+++ project_started/spatial/Recluster_cell_types_Xenium_obj.R
@@ -0,0 +1,144 @@
+# Remove doublets in mouse samples and run harmony on c('Mouse_ID', 'Cohort')
+###libraries
+##################
+library(future)
+
+#plan("multicore", workers = 20)
+#options(future.globals.maxSize = 100 * 1024 ^ 3)
+
+suppressMessages(library(Signac))
+suppressMessages(library(Seurat))
+suppressMessages(library(GenomeInfoDb))
+suppressMessages(library(ggplot2))
+suppressMessages(library(RColorBrewer))
+suppressMessages(library(tidyverse))
+set.seed(1234)
+
+suppressMessages(library(dplyr))
+suppressMessages(library(data.table))
+
+suppressMessages(library(EnsDb.Hsapiens.v86))
+suppressMessages(library(GenomicRanges))
+suppressMessages(library(future))
+suppressMessages(library(optparse))
+suppressMessages(library(BSgenome.Hsapiens.UCSC.hg38))
+
+suppressMessages(library(googlesheets4))
+suppressMessages(library(stringr))
+suppressMessages(library(doParallel))
+suppressMessages(library(harmony))
+
+
+
+################################
+
+#####################################
+####### FUNCTIONS ##################
+####################################
+
+
+
+############################################
+
+###options###
+######################
+option_list = list(
+  make_option(c("-i", "--input.object"),
+              type="character",
+              default=NULL, 
+              help="path to merged object",
+              metavar="character"),
+  make_option(c("-o", "--output"),
+              type="character",
+              default="./", 
+              help="output folder path",
+              metavar="character"),
+  make_option(c("-e", "--extra"),
+              type="character",
+              default="./", 
+              help="add unique string identifier for your data",
+              metavar="character"),
+  make_option(c("-m","--metadata.file"),
+              type="character",
+              default=NULL,
+              help = "path to metadats file with cell types, make cell barcodes in the 1st column",
+              metavar="character"),
+  make_option(c("-c","--cell_type_column"),
+              type="character",
+              default='cell_type',
+              help = "column in the metadata with most recent cell types",
+              metavar="character")
+  
+);
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+###################################
+
+normalizeXenium <- function(xenium.sub) {
+  xenium.sub <- SCTransform(xenium.sub, assay = "Xenium", return.only.var.genes = F, vst.flavor="v2")
+  xenium.sub <- RunPCA(xenium.sub, npcs = num_pcs, features = rownames(xenium.sub))
+  xenium.sub <- RunUMAP(xenium.sub, dims = 1:num_pcs, reduction.name = paste("umap.",num_pcs,"PC", sep = ""), reduction.key = paste("UMAP",num_pcs,"PC_",sep=""))
+  xenium.sub <- FindNeighbors(xenium.sub, reduction = "pca", dims = 1:num_pcs)
+  xenium.sub <- FindClusters(xenium.sub, resolution = 0.3)
+  return(xenium.sub)
+}
+
+# read in initial arguments
+input.path <- opt$input.object
+out_path <- opt$output
+add_filename <- opt$extra
+meta.path <- opt$metadata.file
+cell_column <- opt$cell_type_column
+
+dir.create(out_path, showWarnings = F)
+setwd(out_path)
+
+select <- dplyr::select
+filter <- dplyr::filter
+my.metadata <- fread(meta.path, data.table = F, header = TRUE) %>% 
+  data.frame(row.names = 1, check.rows = F, check.names = F) 
+
+panc.my <- readRDS(input.path)
+panc.my <- AddMetaData(panc.my, my.metadata)
+panc.my@images[[1]] <- NULL
+
+cell.types.oi <- c( 'Hepatocytes', 'Cholangiocytes', 'HSCs|fibro',  'LSECs')
+
+cell.types.in.object <- unique(as.character(unlist(panc.my[[cell_column]])))
+
+
+cell.types.oi %>% walk (function(ct) {
+  
+  if(!file.exists(paste0(add_filename,"_",make.names(ct), ".rds"))) {
+    print(ct)
+    
+    DefaultAssay(int.sub) <- 'Xenium'
+    int.sub <- DietSeurat(int.sub, assays = 'Xenium')
+    int.sub <- subset(x = panc.my, 
+                      cells = rownames(dplyr::filter(panc.my@meta.data, 
+                                                     grepl(ct, .data[[cell_column]])
+                      )
+                      )
+    )
+    print(dim(int.sub))
+    
+    int.sub <- normalizeXenium(int.sub)
+    
+    cat('saving the object...\n')
+    saveRDS(int.sub,  paste0(add_filename,"_",make.names(ct), ".rds"))
+    
+    DimPlot(int.sub, group.by = 'seurat_clusters', raster = F, label = TRUE)
+    ggsave(glue::glue("Dimplot_seurat_clusters_{ct}.pdf"),
+           height=5,width=8,useDingbats=FALSE,limitsize = FALSE)
+    
+    
+  }
+)
+
+
+
+
+
+
+
