5ecd71ff9d27aa1fd6b18deb37066f8cadab56c5
Alla Karpova <allakarpova@Allas-new-MacBook-Pro.local>
2025-04-02 14:20:46 -0500

    blah

--- project_started/objects/Merge_Xenium_samples_SenNet_v1.2.R
+++ project_started/objects/Merge_Xenium_samples_SenNet_v1.2.R
@@ -185,9 +185,9 @@ if (!file.exists(paste0(length(samples.id),"_Merged_not_normalized_",add_filenam
     RunPCA(assay = 'SCT', do.print = FALSE, verbose = T) %>%
     RunUMAP(dims = 1:30, verbose = T) %>%
     RunHarmony('Patient_ID', reduction = 'pca', assay.use = 'SCT') %>%
-    FindNeighbors(reduction = "harmony", dims = 1:30) %>%
+    FindNeighbors(reduction = "harmony", dims = 1:30) #%>%
     #FindClusters(resolution = 0.5, verbose = FALSE) %>%
-    RunUMAP(reduction = "harmony",reduction.name = 'umap.harmony', reduction.key = 'harmonyUMAP_',  dims = 1:30)
+    #RunUMAP(reduction = "harmony",reduction.name = 'umap.harmony', reduction.key = 'harmonyUMAP_',  dims = 1:30)
   
   
   cat('saving the object...\n')
